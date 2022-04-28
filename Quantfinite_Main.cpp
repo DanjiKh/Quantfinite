110e78d30457e6e3e7260269f6625d816712f246
DanjiKh <danilo.khvorost@gmail.com>
2022-04-28 09:19:48 +0200

    Added:
    1) - PointVsHitbox, HitboxVsHitbox which using "hitbox" structure;
    2) - Standard includes;
    3) - Comments;
    4) - Test program to check collisions.

--- Quantfinite_Main.cpp
+++ Quantfinite_Main.cpp
@@ -1,14 +1,57 @@
+/*      
+            -=< QuantFinite >=-
+
+    Project with using olcPixelGameEngine
+
+    Now without license
+ 
+    More information coming soon...
+*/
+
+//	Game engine includes
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
+//	Standard includes 
+#include <iostream>
 
+//	Structures for game objects
+struct Quant {
+	uint64_t layer;
+	olc::vf2d pos;
+	olc::vf2d size;
+	olc::vf2d scale;
+};
+
+//	Main game Class
 class QuantFinite : public olc::PixelGameEngine
 {
+private:
+	std::vector<Quant> m_char;
+	uint64_t m_char_layer = 0;
+
 public:
 	QuantFinite()
 	{
 		sAppName = "QuantFinite";
-	}
+	};
+
+	struct hitbox {
+		olc::vf2d pos;
+		olc::vf2d size;
+	};
+
+	bool PointVsHitbox(const olc::vf2d& p, const hitbox& hb) {
+		return (p.x >= hb.pos.x 
+			&& p.y >= hb.pos.y 
+			&& p.x < hb.pos.x + hb.size.x 
+			&& p.y < hb.pos.y + hb.size.y); 		
+	};
+
+	bool HitboxVsHitbox(const hitbox& hb1, const hitbox& hb2) {
+		return (hb1.pos.x < hb2.pos.x + hb2.size.x && hb1.pos.x + hb1.size.x > hb2.pos.x &&
+				hb1.pos.y < hb2.pos.y + hb2.size.y && hb1.pos.y + hb1.size.y > hb2.pos.y);
+	};
 
 public:
 	bool OnUserCreate() override
@@ -19,7 +62,20 @@ public:
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-		
+		olc::vf2d pMouse = {float(GetMouseX()), float(GetMouseY())};
+		hitbox hb = {{100.0f, 100.0f}, {50.0f, 50.0f}};
+
+		hitbox hb2 = {pMouse, {10.0f, 40.0f}};
+
+		Clear(olc::DARK_BLUE);
+
+		DrawRect(hb2.pos, hb2.size, olc::GREEN);
+
+		if(HitboxVsHitbox(hb2, hb))
+			DrawRect(hb.pos, hb.size, olc::YELLOW);
+		else
+			DrawRect(hb.pos, hb.size, olc::WHITE);
+
 		return true;
 	}
 };
