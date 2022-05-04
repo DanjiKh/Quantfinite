1017173725aff35bbffe878ac88f9a7e290262d1
DanjiKh <danilo.khvorost@gmail.com>
2022-05-04 12:03:23 +0200

    Started constructing Physics Engine
    1) - Add Axis Aligned Bounding Box;
    2) - Add circle structure;
    3) - Add functions to find intersection with: AABB, Circle.
    
    Also:
    Deleted <build> directory from tracked files.

--- Quantfinite_Main.cpp
+++ Quantfinite_Main.cpp
@@ -16,7 +16,7 @@
 #include <iostream>
 
 
-//	Main game Class
+//	Game Class
 class QuantFinite : public olc::PixelGameEngine
 {
 private:
@@ -32,7 +32,20 @@ public:
 
 //	Structures for game objects
 public:
-	struct Quant {
+
+	struct AABB 		//Axis Aligned Bounding Box
+	{
+		olc::vf2d min;
+		olc::vf2d max;
+	};
+
+	struct Circle
+	{
+		float r;
+		olc::vf2d pos;
+	};
+
+	/*struct Quant {
 		uint64_t layer;
 		olc::vf2d pos;
 		olc::vf2d size;
@@ -46,12 +59,28 @@ public:
 		olc::vf2d size;
 
 		//	TODO: Create settings for hitbox.
-	};
+	};*/
 
 
 //	Functions 
-public: 
-	bool PointVsHitbox(const olc::vf2d& p, const hitbox& hb) {
+public:
+
+	bool AABBvsAABB( const AABB& a, const AABB& b )
+	{
+  		if( ( a.max.x < b.min.x ) || ( a.min.x > b.max.x ) ) return false;
+  		if( ( a.max.y < b.min.y ) || ( a.min.y > b.max.y ) ) return false;
+		
+  		return true;
+	};
+
+	bool CirclevsCircle( const Cirlce& a, const Circle& b )
+	{
+		float r2 = a.r + b.r;
+  		r2 *= r2;
+  		return r < (a.x + b.x)^2 + (a.y + b.y)^2;
+	};
+	
+	/*bool PointVsHitbox(const olc::vf2d& p, const hitbox& hb) {
 		return (p.x >= hb.pos.x 
 			&& p.y >= hb.pos.y 
 			&& p.x < hb.pos.x + hb.size.x 
@@ -63,48 +92,21 @@ public:
 				hb1.pos.y < hb2.pos.y + hb2.size.y && hb1.pos.y + hb1.size.y > hb2.pos.y);
 	};
 
-	bool RayVsHitbox(const olc::vf2d& ray_origin, const olc::vf2d& ray_dir, const hitbox& target,
-			olc::vf2d& contact_point, olc::vf2d& contact_normal, float& t_hit_near) 
-
+	bool RayVsHitbox() 
 	{
 		//	TODO: Create a normal clollision.
-		olc::vf2d t_near = (target.pos - ray_origin) / ray_dir;
-		olc::vf2d t_far = (target.pos + target.size - ray_origin) / ray_dir;
 		
-		if (t_near.x > t_far.x) std::swap(t_near.x, t_far.x);
-		if (t_near.y > t_far.y) std::swap(t_near.x, t_far.y);
-		
-
-		if (t_near.x > t_far.y || t_near.y > t_far.x) return false;
-
-		t_hit_near = std::max(t_near.x, t_near.y);
-		float t_hit_far = std::min(t_far.x, t_far.y);
-
-		if (t_hit_far < 0) return false;
-
-
-		contact_point = ray_origin + t_hit_near * ray_dir;
-
-		if (t_near.x > t_near.y)
-			if (ray_dir.x < 0)
-				contact_normal = {1,0};
-			else
-				contact_normal = {-1, 0};
-		else if (t_near.x < t_near.y)
-			if (ray_dir.y < 0)
-				contact_normal = {0, 1};
-			else
-				contact_normal = {0, -1};
 
 		return true; 
-	};
+	};*/
 
 
 //	Engine functions
 public:
 	bool OnUserCreate() override
 	{	
-		Quant game_char_shell = {
+		Quant game_char_shell = 
+		{
 			//	TODO: Add character settings.
 		};
 		
@@ -113,7 +115,10 @@ public:
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-		olc::vf2d pMouse = {float(GetMouseX()), float(GetMouseY())};
+
+
+
+		/*olc::vf2d pMouse = {float(GetMouseX()), float(GetMouseY())};
 
 		hitbox hb = {{100.0f, 100.0f}, {50.0f, 50.0f}};
 
@@ -124,20 +129,9 @@ public:
 		
 		DrawLine(ray_point, pMouse, olc::GREEN); 
 
-		olc::vf2d cp, cn;
-		float t;
-
-		if (RayVsHitbox(ray_point, ray_direction, hb, cp, cn, t) && t <= 1.0f) {
-			DrawRect(hb.pos, hb.size, olc::YELLOW);
-			FillCircle(cp, 3, olc::RED);
-			DrawLine(cp, cp + cn * 10);
-		}
-
-		else {
-			DrawRect(hb.pos, hb.size, olc::WHITE);
-		}
 		
-		/*DrawRect(hb2.pos, hb2.size, olc::GREEN);
+		
+		DrawRect(hb2.pos, hb2.size, olc::GREEN);
 
 		if(HitboxVsHitbox(hb2, hb))
 			DrawRect(hb.pos, hb.size, olc::YELLOW);
@@ -148,10 +142,11 @@ public:
 		return true;
 	}	
 };
+
 int main()
 {
 	QuantFinite demo;
-	if (demo.Construct(256, 240, 4, 4))
+	if ( demo.Construct( 256, 240, 4, 4 ) )
 		demo.Start();
 	return 0;
 }
